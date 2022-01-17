cd3c8155d9013261b20b940c1e6d689716af5ca6
Lisa Schäfer <lfs.schaefer@gmail.com#>
2022-01-17 18:35:14 +0100

    initial commit

---
+++ UR10/UR10_DataFile.m
@@ -0,0 +1,196 @@
+% Simscape(TM) Multibody(TM) version: 7.4
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(12).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(12).angle = 0.0;
+smiData.RigidTransform(12).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(12).ID = '';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [0 128 -9.9999999999999998e-13];  % mm
+smiData.RigidTransform(1).angle = 3.1415926535897891;  % rad
+smiData.RigidTransform(1).axis = [2.1213203435596424e-15 0.70710678118654746 -0.70710678118654757];
+smiData.RigidTransform(1).ID = 'AssemblyGround[UR10(2)^UR10-1:Size 4 Joint^UR10(2)_UR10-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [-4.9999999999999997e-12 1379.8 -163.89071053754301];  % mm
+smiData.RigidTransform(2).angle = 3.1415926535897891;  % rad
+smiData.RigidTransform(2).axis = [-0 -0 -1];
+smiData.RigidTransform(2).ID = 'AssemblyGround[UR10(2)^UR10-1:Size 2 Joint^UR10(2)_UR10-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [4.2755394109089995 1053.376197469679 -70.426892812296003];  % mm
+smiData.RigidTransform(3).angle = 2.0943951023931935;  % rad
+smiData.RigidTransform(3).axis = [0.57735026918962284 0.57735026918962518 0.57735026918962928];
+smiData.RigidTransform(3).ID = 'AssemblyGround[UR10(2)^UR10-1:Lower arm^UR10(2)_UR10-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [0 0 0];  % mm
+smiData.RigidTransform(4).angle = 0;  % rad
+smiData.RigidTransform(4).axis = [0 0 0];
+smiData.RigidTransform(4).ID = 'AssemblyGround[UR10(2)^UR10-1:Base^UR10(2)_UR10-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [-9.9999999999989999 674.39999999999998 -101.840710537543];  % mm
+smiData.RigidTransform(5).angle = 1.5707963267948968;  % rad
+smiData.RigidTransform(5).axis = [-1 -5.4999999999999994e-15 -1.4999999999999997e-15];
+smiData.RigidTransform(5).ID = 'AssemblyGround[UR10(2)^UR10-1:Size 3 Joint^UR10(2)_UR10-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [-1.3e-11 1428.0000000000011 -231.39071053754301];  % mm
+smiData.RigidTransform(6).angle = 1.5707963267948968;  % rad
+smiData.RigidTransform(6).axis = [1 2.5e-15 1.4999999999999997e-15];
+smiData.RigidTransform(6).ID = 'AssemblyGround[UR10(2)^UR10-1:Size 2 Joint^UR10(2)_UR10-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [-9.9999999999999998e-13 128 -176.00000000000099];  % mm
+smiData.RigidTransform(7).angle = 0;  % rad
+smiData.RigidTransform(7).axis = [0 0 0];
+smiData.RigidTransform(7).ID = 'AssemblyGround[UR10(2)^UR10-1:Size 4 Joint^UR10(2)_UR10-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [-2.9999999999999997e-12 1312.3 -115.690710537543];  % mm
+smiData.RigidTransform(8).angle = 1.5707963267948968;  % rad
+smiData.RigidTransform(8).axis = [1 -3.4999999999999993e-15 7.499999999999998e-15];
+smiData.RigidTransform(8).ID = 'AssemblyGround[UR10(2)^UR10-1:Size 2 Joint^UR10(2)_UR10-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [67.218811525936999 1454.3505965035631 -254.26439661127898];  % mm
+smiData.RigidTransform(9).angle = 1.5707963267948968;  % rad
+smiData.RigidTransform(9).axis = [1 1.9999999999999998e-15 1.9999999999999998e-15];
+smiData.RigidTransform(9).ID = 'AssemblyGround[UR10(2)^UR10-1:Tool flange^UR10(2)_UR10-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [-2.9999999999999997e-12 207.00000000000298 -176.00000000000099];  % mm
+smiData.RigidTransform(10).angle = 0;  % rad
+smiData.RigidTransform(10).axis = [0 0 0];
+smiData.RigidTransform(10).ID = 'AssemblyGround[UR10(2)^UR10-1:Upper arm^UR10(2)_UR10-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [-13.897775271512 759.08171136274802 -45.519593852709995];  % mm
+smiData.RigidTransform(11).angle = 3.1415926535897905;  % rad
+smiData.RigidTransform(11).axis = [0.70710678118655002 1.4142135623730901e-15 -0.70710678118654502];
+smiData.RigidTransform(11).ID = 'AssemblyGround[UR10(2)^UR10-1:Size 3 Elbow^UR10(2)_UR10-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [0 0 0];  % mm
+smiData.RigidTransform(12).angle = 0;  % rad
+smiData.RigidTransform(12).axis = [0 0 0];
+smiData.RigidTransform(12).ID = 'RootGround[UR10(2)^UR10-1]';
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(8).mass = 0.0;
+smiData.Solid(8).CoM = [0.0 0.0 0.0];
+smiData.Solid(8).MoI = [0.0 0.0 0.0];
+smiData.Solid(8).PoI = [0.0 0.0 0.0];
+smiData.Solid(8).color = [0.0 0.0 0.0];
+smiData.Solid(8).opacity = 0.0;
+smiData.Solid(8).ID = '';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 3.0771648216512841;  % kg
+smiData.Solid(1).CoM = [-0.0076575633265216075 11.617031173843055 11.445266598633824];  % mm
+smiData.Solid(1).MoI = [11504.789623972059 10742.902066407376 10135.844980041029];  % kg*mm^2
+smiData.Solid(1).PoI = [860.54923726039681 -1.9653104274221802 0.87700069957161486];  % kg*mm^2
+smiData.Solid(1).color = [0.88235294117647056 0.88235294117647056 0.88235294117647056];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = 'Size 4 Joint^UR10(2)_UR10*:*Standard';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.72938504402824378;  % kg
+smiData.Solid(2).CoM = [-0.012789401280323371 57.832827596324123 7.5956638088488475];  % mm
+smiData.Solid(2).MoI = [1118.049753228745 885.05465445687685 1034.0253002900567];  % kg*mm^2
+smiData.Solid(2).PoI = [-91.100110568493548 0.13257328400778973 0.15208077940936496];  % kg*mm^2
+smiData.Solid(2).color = [0.88235294117647056 0.88235294117647056 0.88235294117647056];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = 'Size 2 Joint^UR10(2)_UR10*:*Standard';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 2.6685344821740813;  % kg
+smiData.Solid(3).CoM = [-23.617744015534747 22.236182274752998 -4.2755394109121845];  % mm
+smiData.Solid(3).MoI = [2538.2316578461464 48037.072117949043 48037.072117949043];  % kg*mm^2
+smiData.Solid(3).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(3).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = 'Lower arm^UR10(2)_UR10*:*Standard';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(4).mass = 0.70921945933462693;  % kg
+smiData.Solid(4).CoM = [0.0072223047242828582 18.439080324837818 0.47493926530582259];  % mm
+smiData.Solid(4).MoI = [1174.3881022613587 2147.8149428616271 1146.348365286281];  % kg*mm^2
+smiData.Solid(4).PoI = [-0.26951669618329765 -0.056285959445426893 0.081530991372734202];  % kg*mm^2
+smiData.Solid(4).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(4).opacity = 1;
+smiData.Solid(4).ID = 'Base^UR10(2)_UR10*:*Standard';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(5).mass = 1.3344661190033131;  % kg
+smiData.Solid(5).CoM = [10.566712906736432 63.752218406204875 55.774448734112291];  % mm
+smiData.Solid(5).MoI = [2895.5176538697128 2642.6865527199789 2712.9657172801567];  % kg*mm^2
+smiData.Solid(5).PoI = [269.92782473675635 -0.16323762667409977 3.0430444329872688];  % kg*mm^2
+smiData.Solid(5).color = [0.88235294117647056 0.88235294117647056 0.88235294117647056];
+smiData.Solid(5).opacity = 1;
+smiData.Solid(5).ID = 'Size 3 Joint^UR10(2)_UR10*:*Standard';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(6).mass = 0.15951060057187763;  % kg
+smiData.Solid(6).CoM = [-67.218814359112187 15.633621766452638 27.271241387251902];  % mm
+smiData.Solid(6).MoI = [82.229855344842122 149.94376039924992 87.767664156981908];  % kg*mm^2
+smiData.Solid(6).PoI = [0.42726354990060905 -2.288602734852816e-05 -4.1414197868635266e-06];  % kg*mm^2
+smiData.Solid(6).color = [0.88235294117647056 0.88235294117647056 0.88235294117647056];
+smiData.Solid(6).opacity = 1;
+smiData.Solid(6).ID = 'Tool flange^UR10(2)_UR10*:*Standard';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(7).mass = 4.3051218739754322;  % kg
+smiData.Solid(7).CoM = [0 233.73827410463457 0];  % mm
+smiData.Solid(7).MoI = [82156.537838613105 6491.5694543200198 82156.537838613105];  % kg*mm^2
+smiData.Solid(7).PoI = [0 0 0];  % kg*mm^2
+smiData.Solid(7).color = [0.792156862745098 0.81960784313725488 0.93333333333333335];
+smiData.Solid(7).opacity = 1;
+smiData.Solid(7).ID = 'Upper arm^UR10(2)_UR10*:*Standard';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(8).mass = 1.2587992391278511;  % kg
+smiData.Solid(8).CoM = [8.9081752151709424 7.7646433623306379 -13.893761637212771];  % mm
+smiData.Solid(8).MoI = [2509.7437102420463 2395.3066143964293 2570.2021646763237];  % kg*mm^2
+smiData.Solid(8).PoI = [0.8111510900833766 0.50827467407209492 -222.45544951904148];  % kg*mm^2
+smiData.Solid(8).color = [0.88235294117647056 0.88235294117647056 0.88235294117647056];
+smiData.Solid(8).opacity = 1;
+smiData.Solid(8).ID = 'Size 3 Elbow^UR10(2)_UR10*:*Standard';
+
