82577738089b97d210c66059199b84f1531a8ada
Semyon Kondratev <execute.ink@gmail.com>
2022-02-17 22:27:41 +0100

    Update. The Calculations of the torques are wrong

--- compute_torques_with_newton_euler.m
+++ compute_torques_with_newton_euler.m
@@ -14,8 +14,9 @@ omega = zeros(3, n_joints, sim_steps);
 omega_dot = zeros(3, n_joints, sim_steps);
 v_dot = zeros(3, n_joints, sim_steps);
 vs_dot = zeros(3, n_joints, sim_steps);
-tau = zeros(1,n_joints, sim_steps);
+tau = zeros(n_joints, sim_steps);
 
+% concatenate data from simulation for easier iteration
 R = cat(3, R_01, R_12, R_23, R_34, R_45, R_56);
 p = cat(2, p_01', p_12', p_23', p_34', p_45', p_56');
 s = cat(2, s1', s2', s3', s4', s5', s6');
@@ -28,13 +29,14 @@ m = m';
 for i = 1:sim_steps
 	[omega(:,:,i), omega_dot(:,:,i), v_dot(:,:,i), vs_dot(:,:,i)] = ...
 		compute_kinematics(q(:,i), q_dot(:,i), q_2dot(:,i), R, R_W0(:,:,i), p, s);
-	tau(:,:,i) = compute_forces_and_torques(omega(:,:,i), omega_dot(:,:,i), vs_dot(:,:,i), m, Ic, p, s, R);
+	tau(:,i) = compute_forces_and_torques(omega(:,:,i), omega_dot(:,:,i), vs_dot(:,:,i), m, Ic, p, s, R);
 end
 
 
 % plot(tout(:,1), squeeze(omega(3,1,:)));
 % plot(tout, squeeze(omega_dot(3,1,:)));
-plot(tout, tau_sim);
+% plot(tout, tau_sim);
+ts = timeseries(tau, tout, 'Name', 'tau');
 
 
 function [omega, omega_dot, v_dot, vs_dot] = compute_kinematics(q, q_dot, q_2dot, R, R_W0, p, s)
@@ -50,7 +52,7 @@ function [omega, omega_dot, v_dot, vs_dot] = compute_kinematics(q, q_dot, q_2dot
 	
 	v = zeros(3,6);
 	v_dot = zeros(3,6);
-	v_dot(:,1) = inv(R_W0) * [0 -9.81 0]'; % acceleration of Coordinate System
+	v_dot(:,1) = inv(R_W0) * [0 -9.81 0]'; % acceleration of Coordinate System	
 	vs_dot(:,1) = v_dot(:,1); % acceleration of center of mass (COM)	
 	
 	for i = 1:n_joints-1
